0bd8c35b3109969078bf8a2f3a98b50cb77abf30
juanf03 <juanf03@gmail.com>
2016-09-16 07:39:08 +0000

    deploy heroku

---
+++ app.js
@@ -0,0 +1,50 @@
+var express=require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var urlConverter=require('./modules/url-converter');
+
+
+
+var app = express();
+
+// Path to our public directory
+
+var pub = __dirname;
+app.use(express.static(pub));
+
+
+// Set our default template engine to "jade"
+// which prevents the need for extensions
+// (although you can still mix and match)
+app.set('view engine', 'jade');
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/* GET home page. */
+app.get('/',function(req,res,next){
+  
+    res.render('index', { title: 'Express' });
+  
+});
+
+
+app.get('/:id', function(req, res, next) {
+  
+   var id=req.params.id;
+     urlConverter.redirectToShortURL(id,res);
+
+});
+
+
+app.get('/new/*', function(req, res, next) {
+
+  var url = req.url.replace('/new/','');
+
+  urlConverter.shortenURL(url).then(obj=> {return res.json(obj);});
+
+});
+
+
+
+app.listen(process.env.PORT || 5000);
---
+++ modules/url-converter.js
@@ -0,0 +1,83 @@
+var mongo = require('mongodb');
+var assert = require('assert');
+var Promise = require("bluebird");
+
+var mongoUrl="mongodb://localhost:27017/urls";
+
+
+
+module.exports = {
+  validateURL:function(url){
+    
+    var regex=/^((https?):\/\/)?([w|W]{3}\.)+[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
+    return regex.test(url);
+    
+},
+  genRandId:function(){
+     var num = Math.floor(100000 + Math.random() * 900000);
+     
+    return num.toString().substring(0, 4);
+  },
+    shortenURL: function(url) {
+    
+
+  if(this.validateURL(url)){
+
+    var randId=this.genRandId();
+    var shortenedUrl="https://urlshortenertool.herokuapp.com/"+randId;
+    
+      var newUrl ={
+        originalUrl: url,
+        short_url: shortenedUrl,
+        urlNumber:randId
+      }
+          //insert in mongo and puth the id of mongo after the url of the second parameter
+    var responseObject= {original_url: url};
+    
+    
+   return mongo.MongoClient.connect(mongoUrl)
+     
+     .then(function(db,err){
+       if (err) {
+       throw new Error('Database failed to connect!');  //
+      } else {
+           
+        return   db.collection('urls').insertOne(newUrl)
+           .then((result,err)=>{
+               if (err) {
+          
+            throw new Error('Insert Failed!');
+          }
+          console.log('Item inserte with id: '+newUrl._id);
+          db.close();
+          responseObject.short_url=shortenedUrl;
+          return responseObject;
+     
+           })
+           
+           ;
+        
+      }
+       
+     })
+ 
+      .then(resObj=>{
+             return resObj;
+      });
+    
+  }
+  
+  },
+  redirectToShortURL:function(id,res){
+
+   // check mongo database for the id, get the url  related to that id and redirect the user to that url   
+      mongo.connect(mongoUrl,function(err,db){
+        assert.equal(null,err);
+        db.collection('urls').findOne({ urlNumber : id }, (err, document)=> {
+           console.log("Redirecting user to:"+ document.originalUrl);
+           res.redirect(document.originalUrl);
+        });
+    
+      }); 
+  }
+};
